9c78ab7698b77658bb38aa47ee0b6463dd02bc11
Ashtalakshmi8 <ASHTALAKSHMI8>
2021-12-20 22:49:14 +0530

    ashu

---
+++ short.java
@@ -0,0 +1,12 @@
+class am {
+      public static void main (String [] args ){
+      byte a;
+      a=70;
+      System.out.println (a);
+
+      short Bankbalance;
+      Bankbalance=32;
+      System.out.println (Bankbalance);
+}
+}
+
